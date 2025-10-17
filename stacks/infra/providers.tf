ccd6fe4dda3619fe7f287ffb6ea5e074bfecbf25
krishna.bhargav <krishna.bhargav@infor.com>
2025-10-17 16:13:14 +0200

    Enable aws-auth

--- stacks/infra/providers.tf
+++ stacks/infra/providers.tf
@@ -1,21 +1,3 @@
-# --- AWS provider (keep as-is) ---
 provider "aws" {
   region = var.region
-}
-
-# --- Read EKS connection details from the cluster you create in this stack ---
-# (Assumes your EKS module block is named "eks-core"; if it's "eks" change the name below.)
-data "aws_eks_cluster" "this" {
-  name = module.eks-core.cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks-core.cluster_name
-}
-
-# --- Kubernetes provider that aws-auth (and other K8s resources) will use ---
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
 }
