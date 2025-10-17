408144fc9ebd7a648005bcdb03f80ff02cb442f0
krishna.bhargav <krishna.bhargav@infor.com>
2025-10-18 01:41:10 +0200

    Updated provider to talk to eks

--- stacks/infra/providers.tf
+++ stacks/infra/providers.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = var.region
 }
+
+#############################################
+# Kubernetes provider (talk to EKS directly)
+#############################################
+provider "kubernetes" {
+  host                   = module.eks_core.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_core.cluster_ca)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_core.cluster_name, "--region", var.region]
+  }
+}
