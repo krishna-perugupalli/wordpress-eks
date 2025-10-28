b468d42a46a1ef2ba877c196ef51943f782e3c98
krishna.bhargav <krishna.bhargav@infor.com>
2025-10-28 11:56:58 +0100

    Fix for duplicate provider error

--- stacks/infra/providers.tf
+++ stacks/infra/providers.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -13,12 +9,17 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    args = [
+      "eks", "get-token",
+      "--cluster-name", module.eks.cluster_name,
+      "--region", var.region
+    ]
+    env = {
+      # These two help both locally (SSO) and in CI
+      AWS_STS_REGIONAL_ENDPOINTS = "regional"
+      AWS_SDK_LOAD_CONFIG        = "1"
+      # Optional (local): if you use SSO profiles, pass it through
+      # AWS_PROFILE                = var.aws_profile
+    }
   }
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
 }
