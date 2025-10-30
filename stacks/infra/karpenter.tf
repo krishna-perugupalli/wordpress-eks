52f441d6ce36387c76d9138366bf79ead24c96fd
krishna.bhargav <krishna.bhargav@infor.com>
2025-10-30 10:51:27 +0100

    Removed index from karpetner module

--- stacks/infra/karpenter.tf
+++ stacks/infra/karpenter.tf
@@ -23,7 +23,7 @@ module "karpenter" {
   source                        = "terraform-aws-modules/eks/aws//modules/karpenter"
   version                       = "20.24.0"
   cluster_name                  = module.eks.cluster_name
-  irsa_oidc_provider_arn        = module.eks[0].oidc_provider_arn
+  irsa_oidc_provider_arn        = module.eks.oidc_provider_arn
   iam_role_name                 = "${local.name}karpenter-controller"
   iam_role_use_name_prefix      = false
   enable_irsa                   = true
@@ -31,7 +31,7 @@ module "karpenter" {
   node_iam_role_use_name_prefix = false
   create_instance_profile       = true
   node_iam_role_additional_policies = {
-    "KMSPolicy" = aws_iam_policy.karpenter_node_role_kms_policy[0].arn
+    "KMSPolicy" = aws_iam_policy.karpenter_node_role_kms_policy.arn
   }
 }
 
