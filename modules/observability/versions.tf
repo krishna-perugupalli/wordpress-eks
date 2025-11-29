ed89befc0d9d6e5980d87c728e083755c00144e5
Bhargav Krishna Perugupalli <krishna@accelyte.com>
2025-11-29 22:54:56 +0100

    Added missing kubectl provider to observability module

--- modules/observability/versions.tf
+++ modules/observability/versions.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.13"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
